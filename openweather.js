f6522fbdd95ff6a1deb7bb4c22df4893929e17c9
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2021-11-04 16:44:50 +0900

    🔁 changed weather api

---
+++ openweather.js
@@ -0,0 +1,58 @@
+
+const getJson = function(url,callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET',url,true);
+    xhr.responseType = 'json';
+    xhr.onload = function(){
+        const status = xhr.status;
+        if(status == 200){
+            console.log(xhr.response);
+        }else{
+            console.log(xhr.response);
+        }
+    };
+    xhr.send();
+
+    
+};
+
+
+
+
+
+getJson('http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=49f2a44ba633f02db2d706240b4c42d4'),
+function(err,data){
+    if(err!== null){
+        alert('sorry,');
+    }else{
+        loadWeather(data);
+    }
+}
+
+function loadWeather(data){
+    let location = document.querySelector('.location');
+    let currentTime = document.querySelector('.currenct-time');
+    let currentTemp = document.querySelector('.current-temp');
+    let feelsLike = document.querySelector('.feels-like');
+    let minTemp = document.querySelector(',min-temp');
+    let maxTemp = document.querySelector('.max-temp');
+    let icon = document.querySelector('.icon');
+    let weatherIcon = data.weather[0].icon;
+
+    let date = new Date();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    location.append(data.name);
+    currentTemp.append('$(data.min.temp}');
+    feelsLike.append('${data.main.feels_like}');
+    maxTemp.append('최고 ${data.main.temp_max}');
+    minTemp.append('최저 ${data.main.temp_min}');
+
+    icon.innerHTML = '<img src = http://openweathermap.org/img/wn/${weatherIcon}.png>';
+
+    currentTime.append('${month}월 ${day}일 ${hours} : ${minutes}');
+
+}
